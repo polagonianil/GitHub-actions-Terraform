32677ae85227b71fd5b31aaa1cad23a729980725
anil <polagonianilgoud@gmail.com>
2024-11-04 17:43:40 +0530

    variable file modified

--- variable.tf
+++ variable.tf
@@ -1,4 +1,11 @@
 variable "instance_type" {
-  description = "The instance type"
-  type        = string  # Specify the type
+  description = "Type of the EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "region" {
+  description = "AWS region for deployment"
+  type        = string
+  default     = "us-east-1"
 }
