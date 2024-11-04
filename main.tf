f588c168e67eb02b33825daf533a14c71dd4b044
anil <polagonianilgoud@gmail.com>
2024-11-04 14:28:54 +0530

    sixth commit

--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ provider "aws" {
 resource "aws_instance" "foo" {
   ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = var.instance_type
+
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  root_block_device {
+    encrypted = true
+  }
+
   tags = {
       Name = "TF-Instance"
   }
